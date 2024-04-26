dc00b32935463f15ba788512c35f37368f9109c7
Furkan Ayhan <halimayhan25@gmail.com>
2024-04-26 00:53:16 -0400

    added projects

--- src/Pages/projects/Projects.js
+++ src/Pages/projects/Projects.js
@@ -1,11 +1,66 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import devpic from "../../images/devpic.png"
+import ProjectCard from "./ProjectCard";
+import "./Projects.css"
 
-const Projects = () =>{
-    return (
-        <div>
-            <h1> MY PROJECTS</h1>
-        </div>
-    )
+function Projects() {
+  return (
+    <Container fluid>
+      <Container>
+        <h1 style={{ color: "white", textAlign: "center", paddingTop: "5%"}}>
+          My Projects
+        </h1>
+        <Row style={{ justifyContent: "center", paddingBottom: "5%" }}>
+          <Col md={4} className = "card-cols" >
+            <ProjectCard className = "card"
+              imgPath={devpic}
+              title="Movies App"
+              description="Crafted a user-friendly movies app using Node.js and React"
+              ghLink="https://github.com/fayhan25/MoviesApp"
+            />
+          </Col>
+
+          <Col md={4} className = "card-cols">
+            <ProjectCard className = "card" 
+
+              imgPath={devpic}
+              title="Places App"
+              description="Developed a dynamic Places app with the MERN framework (MongoDB, ExpressJS, React, NodeJs) for users to share and review visited places."
+              ghLink="https://github.com/fayhan25/placesapp"
+            />
+          </Col>
+
+          <Col md={4} className = "card-cols">
+            <ProjectCard className = "card"
+              imgPath={devpic}
+              title="HRT Fitness"
+              description="Developed a dynamic fitness app with Java and Android Studio, offering personalized diet and workout recommendations."
+              ghLink="https://github.com/fayhan25/HRTFitnessApp"          
+            />
+          </Col>
+
+          <Col md={4} className = "card-cols">
+            <ProjectCard className = "card"
+              imgPath={devpic}
+              title="FlixList"
+              description="Created a dynamic movie review platform with JavaScript/React, enabling users to share reviews with friends and followers."
+              ghLink="https://github.com/fayhan25/FlixList"
+            />
+          </Col>
+
+          <Col md={4} className = "card-cols">
+            <ProjectCard className = "card"
+              imgPath={devpic}
+              title="Portfolio"
+              description="Portfolio built with reactjs to present information about myself"
+              ghLink="https://github.com/fayhan25/Portfolio"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
 }
 
-export default Projects
+export default Projects;

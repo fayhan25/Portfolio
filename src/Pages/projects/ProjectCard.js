5d5bfc97fd0eeb5c12bfeeaed5914b6aee7fd2f5
Furkan Ayhan <halimayhan25@gmail.com>
2024-04-28 02:19:28 -0400

    made changes to projects tab

--- src/Pages/projects/ProjectCard.js
+++ src/Pages/projects/ProjectCard.js
@@ -6,8 +6,8 @@ import { BsGithub } from "react-icons/bs";
 
 const ProjectCard = (props) =>{
     return (
-        <Card style = {{backgroundColor: "#9370DB"}}>
-        <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+        <Card style = {{backgroundColor: "#9370DB", height: '100%'}}>
+        <Card.Img style = {{width: '100%', height: '60%'}} variant="top" src={props.imgPath} alt="card-img" />
         <Card.Body>
           <Card.Title style = {{color: "white"}}>{props.title}</Card.Title>
           <Card.Text style={{ textAlign: "justify", color: "white" }}>
@@ -15,12 +15,11 @@ const ProjectCard = (props) =>{
           </Card.Text>
           <Button variant="primary" href={props.ghLink} target="_blank">
             <BsGithub /> &nbsp;
+            Github
           </Button>
           {"\n"}
           {"\n"}
-  
-          {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
-  
+
           {props.demoLink && (
             <Button
               variant="primary"
--- src/Pages/projects/Projects.js
+++ src/Pages/projects/Projects.js
@@ -3,7 +3,8 @@ import { Container, Row, Col } from "react-bootstrap";
 import devpic from "../../images/devpic.png"
 import ProjectCard from "./ProjectCard";
 import "./Projects.css"
-
+import Movie from '../../images/Movie.PNG'
+import places from '../../images/places.PNG'
 function Projects() {
   return (
     <Container fluid>
@@ -13,49 +14,61 @@ function Projects() {
         </h1>
         <Row style={{ justifyContent: "center", paddingBottom: "5%" }}>
           <Col md={4} className = "card-cols" >
-            <ProjectCard className = "card"
-              imgPath={devpic}
+            <div className="project-container" style={{height: '100%'}}>
+            <ProjectCard className = "card" 
+              imgPath={Movie}
               title="Movies App"
               description="Crafted a user-friendly movies app using Node.js and React"
               ghLink="https://github.com/fayhan25/MoviesApp"
             />
+             </div>
           </Col>
-
+          
           <Col md={4} className = "card-cols">
+            
             <ProjectCard className = "card" 
-
-              imgPath={devpic}
+              imgPath={places}
               title="Places App"
               description="Developed a dynamic Places app with the MERN framework (MongoDB, ExpressJS, React, NodeJs) for users to share and review visited places."
               ghLink="https://github.com/fayhan25/placesapp"
             />
+            
+
           </Col>
 
           <Col md={4} className = "card-cols">
+            <div className="project-container">
             <ProjectCard className = "card"
               imgPath={devpic}
               title="HRT Fitness"
               description="Developed a dynamic fitness app with Java and Android Studio, offering personalized diet and workout recommendations."
               ghLink="https://github.com/fayhan25/HRTFitnessApp"          
             />
+            </div>
+
           </Col>
 
           <Col md={4} className = "card-cols">
+            <div className="project-container">
             <ProjectCard className = "card"
               imgPath={devpic}
               title="FlixList"
               description="Created a dynamic movie review platform with JavaScript/React, enabling users to share reviews with friends and followers."
               ghLink="https://github.com/fayhan25/FlixList"
             />
+            </div>
+
           </Col>
 
           <Col md={4} className = "card-cols">
+            <div className="project-container">
             <ProjectCard className = "card"
               imgPath={devpic}
               title="Portfolio"
               description="Portfolio built with reactjs to present information about myself"
               ghLink="https://github.com/fayhan25/Portfolio"
             />
+            </div>
           </Col>
         </Row>
       </Container>
